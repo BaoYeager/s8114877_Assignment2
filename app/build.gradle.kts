061beb7a2f90b2fa8f845170388aaabe33e62695
Gia Bao <giabaotr6969@gmail.com>
2025-10-19 22:19:57 +1100

    add 'back' bottom button to go back dashboard from detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,14 @@ plugins {
 
 android {
 
+
     namespace = "com.example.s8114877_assignment2"
     compileSdk = 36
 
     defaultConfig {
+        vectorDrawables {
+            useSupportLibrary = true
+        }
         applicationId = "com.example.s8114877_assignment2"
         minSdk = 24
         targetSdk = 36
@@ -64,6 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.google.android.material:material:1.12.0")
     // Retrofit & Gson
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
--- app/src/main/java/com/example/s8114877_assignment2/ui/DetailFragment.kt
+++ app/src/main/java/com/example/s8114877_assignment2/ui/DetailFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.s8114877_assignment2.R
 import com.example.s8114877_assignment2.ui.Adapter.DetailAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.json.JSONObject
 
 class DetailFragment : Fragment() {
@@ -32,6 +34,18 @@ class DetailFragment : Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewDetail)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = DetailAdapter(entity)
+
+        // Xử lý bottom navigation
+        val bottomNav = view.findViewById<BottomNavigationView>(R.id.detailBottomNav)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_back -> {
+                    findNavController().navigateUp() // Quay lại Dashboard
+                    true
+                }
+                else -> false
+            }
+        }
 
         return view
     }
